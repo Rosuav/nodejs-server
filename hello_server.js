f37229273892f4be188b9c41c33f2940a2757ccb
Chris Angelico <rosuav@gmail.com>
2016-05-26 06:29:17 +1000

    Toss in another route

--- hello_server.js
+++ hello_server.js
@@ -6,5 +6,12 @@ app.get('/', function(request, response) {
     response.send("Hello World");
 });
 
+app.get('/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    //No string formatting in JS?
+    response.send(["Hello", first, last].join(" "));
+});
+
 //app.listen(process.env.PORT, process.env.IP);
 app.listen(8800, "0.0.0.0");
